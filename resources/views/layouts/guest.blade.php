1ab96bae32db84bab940bcb167f4554a5d0f7f87
Labibluthfi11 <muhammadlabibluthfitrpl@gmail.com>
2026-01-14 10:37:49 +0700

    menambahkan logo ansel di browser

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name','Absensi Ansel Muda Berkarya') }}</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
