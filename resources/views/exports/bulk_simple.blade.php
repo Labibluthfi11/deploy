5d04143bfdba990c140659f2da3711c6cc3e77dd
Labibluthfi11 <muhammadlabibluthfitrpl@gmail.com>
2026-01-19 13:58:07 +0700

    membenarkan penarikan absen

--- resources/views/exports/bulk_simple.blade.php
+++ resources/views/exports/bulk_simple.blade.php
@@ -22,6 +22,19 @@
             <tr>
                 <td colspan="15"></td> {{-- Spasi --}}
             </tr>
+
+            {{-- HEADER TABEL - CUMA SEKALI --}}
+            <tr style="background-color: #D9D9D9;">
+                <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 50px;">NO</th>
+                <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 200px;">Nama</th>
+                @for($i = 1; $i <= 11; $i++)
+                    <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 100px;">
+                        {{ str_pad($i, 2, '0', STR_PAD_LEFT) }}-Jan
+                    </th>
+                @endfor
+                <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 100px;">Total Telat</th>
+                <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 120px;">Total Lembur</th>
+            </tr>
         </thead>
 
         {{-- BODY TABEL --}}
@@ -35,7 +48,7 @@
                     // Split tanggal jadi chunk 11 hari
                     $dateChunks = array_chunk($group['dates'], 11);
 
-                    // Hitung total telat & lembur UNTUK SELURUH BULAN INI
+                    // Hitung total telat & lembur
                     $totalTelat = 0;
                     $totalMenitLembur = 0;
 
@@ -52,57 +65,56 @@
                         }
                     }
 
-                    // Format total lembur
                     $lemburJam = floor($totalMenitLembur / 60);
                     $lemburMenit = $totalMenitLembur % 60;
                     $totalLemburStr = $totalMenitLembur > 0 ? sprintf('%dj %dm', $lemburJam, $lemburMenit) : '-';
+
+                    // Total baris = 2 baris per chunk (tanggal + data)
+                    $totalRows = count($dateChunks) * 2;
                 @endphp
 
-                {{-- LOOP SETIAP CHUNK TANGGAL UNTUK USER INI --}}
+                {{-- LOOP SETIAP CHUNK --}}
                 @foreach($dateChunks as $chunkIndex => $chunk)
-                    {{-- HEADER ROW: TANGGAL --}}
-                    <tr style="background-color: #D9D9D9;">
+                    {{-- BARIS TANGGAL --}}
+                    <tr>
+                        {{-- NO & NAMA di rowspan (cuma di chunk pertama) --}}
                         @if($chunkIndex === 0)
-                            <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 50px;">NO</th>
-                            <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 200px;">Nama</th>
-                        @else
-                            <th style="border: 1px solid #000000;"></th>
-                            <th style="border: 1px solid #000000;"></th>
+                            <td rowspan="{{ $totalRows }}" style="text-align: center; border: 1px solid #000000; vertical-align: middle; font-weight: bold;">{{ $no }}</td>
+                            <td rowspan="{{ $totalRows }}" style="text-align: left; border: 1px solid #000000; padding-left: 5px; vertical-align: middle; font-weight: bold;">{{ $user->name }}</td>
                         @endif
 
+                        {{-- ISI TANGGAL (01-Jan, 02-Jan, dst) --}}
                         @for($i = 0; $i < 11; $i++)
-                            @if(isset($chunk[$i]))
-                                <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 100px;">
-                                    {{ $chunk[$i]->format('d-M') }}
-                                </th>
-                            @else
-                                <th style="border: 1px solid #000000; background-color: #F0F0F0;"></th>
-                            @endif
+                            @php
+                                if (isset($chunk[$i])) {
+                                    $date = $chunk[$i];
+                                    $cellContent = $date->format('d-M');
+                                } else {
+                                    $cellContent = '';
+                                }
+                            @endphp
+                            <td style="text-align: center; border: 1px solid #000000; font-size: 10px;">{{ $cellContent }}</td>
                         @endfor
 
+                        {{-- TOTAL (rowspan, cuma di chunk pertama) --}}
                         @if($chunkIndex === 0)
-                            <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 100px;">Total Telat</th>
-                            <th style="font-weight: bold; text-align: center; border: 1px solid #000000; width: 120px;">Total Lembur</th>
-                        @else
-                            <th style="border: 1px solid #000000;"></th>
-                            <th style="border: 1px solid #000000;"></th>
+                            <td rowspan="{{ $totalRows }}" style="text-align: center; border: 1px solid #000000; font-weight: bold; vertical-align: middle;">
+                                {{ $totalTelat > 0 ? $totalTelat . 'x' : '-' }}
+                            </td>
+                            <td rowspan="{{ $totalRows }}" style="text-align: center; border: 1px solid #000000; font-weight: bold; vertical-align: middle;">
+                                {{ $totalLemburStr }}
+                            </td>
                         @endif
                     </tr>
 
-                    {{-- DATA ROW: ISI ABSENSI --}}
+                    {{-- BARIS DATA --}}
                     <tr>
-                        @if($chunkIndex === 0)
-                            <td rowspan="{{ count($dateChunks) * 2 }}" style="text-align: center; border: 1px solid #000000; vertical-align: middle; font-weight: bold;">{{ $no }}</td>
-                            <td rowspan="{{ count($dateChunks) * 2 }}" style="text-align: left; border: 1px solid #000000; padding-left: 5px; vertical-align: middle; font-weight: bold;">{{ $user->name }}</td>
-                        @endif
-
-                        {{-- LOOP 11 TANGGAL DATA --}}
+                        {{-- ISI DATA ABSENSI --}}
                         @for($i = 0; $i < 11; $i++)
                             @php
                                 if (isset($chunk[$i])) {
                                     $date = $chunk[$i];
 
-                                    // Cari absensi di tanggal ini
                                     $absen = $userAbsensi->first(function($item) use ($date) {
                                         return \Carbon\Carbon::parse($item->check_in_at)->isSameDay($date);
                                     });
@@ -111,33 +123,19 @@
                                     $cellStyle = 'text-align: center; border: 1px solid #000000; vertical-align: middle; font-size: 10px;';
 
                                     if ($absen) {
-                                        // Ada absensi
                                         $checkIn = \Carbon\Carbon::parse($absen->check_in_at)->format('H:i');
                                         $checkOut = $absen->check_out_at ? \Carbon\Carbon::parse($absen->check_out_at)->format('H:i') : '-';
 
                                         if (strtolower($absen->status) === 'hadir') {
                                             $cellContent = "$checkIn - $checkOut";
 
-                                            // TELAT INFO
-                                            $telatMenit = $absen->late_minutes ?? 0;
-                                            if ($telatMenit > 0) {
-                                                $telatJam = floor($telatMenit / 60);
-                                                $telatSisa = $telatMenit % 60;
-                                                if ($telatJam > 0) {
-                                                    $telatStr = sprintf('Telat: %dj %dm', $telatJam, $telatSisa);
-                                                } else {
-                                                    $telatStr = sprintf('Telat: %dm', $telatSisa);
-                                                }
-                                                $cellContent .= "\n$telatStr";
-                                            }
-
-                                            // LEMBUR INFO
+                                            // Lembur
                                             $lemburMenit = $absen->overtime_minutes ?? 0;
                                             if ($lemburMenit > 0) {
                                                 $lemburJam = floor($lemburMenit / 60);
                                                 $lemburSisa = $lemburMenit % 60;
                                                 $lemburStr = sprintf('Lembur: %dj %dm', $lemburJam, $lemburSisa);
-                                                $cellContent .= "\n$lemburStr";
+                                                $cellContent .= " $lemburStr";
                                             }
                                         } elseif (strtolower($absen->status) === 'izin') {
                                             $cellContent = 'Izin';
@@ -147,7 +145,6 @@
                                             $cellContent = ucfirst($absen->status);
                                         }
                                     } else {
-                                        // Ga ada absensi
                                         $isWeekday = $date->dayOfWeek >= 1 && $date->dayOfWeek <= 5;
 
                                         if ($isWeekday) {
@@ -158,7 +155,6 @@
                                         }
                                     }
                                 } else {
-                                    // Ga ada tanggal lagi
                                     $cellContent = '';
                                     $cellStyle = 'text-align: center; border: 1px solid #000000; background-color: #F0F0F0;';
                                 }
@@ -166,16 +162,6 @@
 
                             <td style="{{ $cellStyle }}">{{ $cellContent }}</td>
                         @endfor
-
-                        {{-- TOTAL TELAT & LEMBUR --}}
-                        @if($chunkIndex === 0)
-                            <td rowspan="{{ count($dateChunks) * 2 }}" style="text-align: center; border: 1px solid #000000; font-weight: bold; vertical-align: middle;">
-                                {{ $totalTelat > 0 ? $totalTelat . 'x' : '-' }}
-                            </td>
-                            <td rowspan="{{ count($dateChunks) * 2 }}" style="text-align: center; border: 1px solid #000000; font-weight: bold; vertical-align: middle;">
-                                {{ $totalLemburStr }}
-                            </td>
-                        @endif
                     </tr>
                 @endforeach
 
