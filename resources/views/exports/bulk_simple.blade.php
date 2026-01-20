7dc699ef434767c0dd6743a014c6666270938e93
Labibluthfi11 <muhammadlabibluthfitrpl@gmail.com>
2026-01-20 11:04:21 +0700

    membenarkan penarikan absen

--- resources/views/exports/bulk_simple.blade.php
+++ resources/views/exports/bulk_simple.blade.php
@@ -23,25 +23,11 @@
         </tr>
         <tr><td colspan="14"></td></tr>
 
-        {{-- HEADER TANGGAL PERTAMA --}}
+        {{-- HEADER KOLOM --}}
         <tr style="background:#D9D9D9;">
+            <th style="border:1px solid #000; width:50px;">NO</th>
             <th style="border:1px solid #000; width:200px;">Nama</th>
-
-            @php
-                $allDates = $group['dates'];
-                $first11 = array_slice($allDates, 0, 11);
-            @endphp
-
-            @foreach($first11 as $date)
-                <th style="border:1px solid #000; text-align:center; width:90px;">
-                    {{ \Carbon\Carbon::parse($date)->format('d-M') }}
-                </th>
-            @endforeach
-
-            @for($i = count($first11); $i < 11; $i++)
-                <th style="border:1px solid #000;"></th>
-            @endfor
-
+            <th colspan="11" style="border:1px solid #000; text-align:center;">Tanggal Dan Bulan</th>
             <th style="border:1px solid #000; width:100px;">Total Telat</th>
             <th style="border:1px solid #000; width:120px;">Total Lembur</th>
         </tr>
@@ -51,11 +37,13 @@
     {{-- BODY --}}
     {{-- ======================= --}}
     <tbody>
-        @foreach($users as $user)
+        @foreach($users as $userIndex => $user)
             @php
                 $userAbsensi = $absensiData->where('user_id', $user->id);
+                $allDates = $group['dates'];
                 $dateChunks = array_chunk($allDates, 11);
 
+                // Hitung total telat & lembur untuk dates di group ini
                 $totalTelat = 0;
                 $totalMenitLembur = 0;
 
@@ -74,40 +62,48 @@
                 $lemburMenit = $totalMenitLembur % 60;
                 $totalLemburStr = $totalMenitLembur ? "{$lemburJam}j {$lemburMenit}m" : '-';
 
-                $totalChunks = count($dateChunks);
-                $rowSpan = 1 + (($totalChunks - 1) * 2);
+                // Rowspan = jumlah chunk * 2 (header tanggal + data)
+                $rowSpan = count($dateChunks) * 2;
             @endphp
 
             @foreach($dateChunks as $chunkIndex => $chunk)
-
-                {{-- HEADER SELIPAN --}}
-                @if($chunkIndex > 0)
+                {{-- ROW HEADER TANGGAL --}}
                 <tr>
-                    <td style="border:1px solid #000; background:#f2f2f2;"></td>
-
-                    @foreach($chunk as $date)
-                        <td style="border:1px solid #000; font-weight:bold; text-align:center; background:#f2f2f2;">
-                            {{ \Carbon\Carbon::parse($date)->format('d-M') }}
-                        </td>
-                    @endforeach
-
-                    @for($k = count($chunk); $k < 11; $k++)
-                        <td style="border:1px solid #000; background:#eaeaea;"></td>
-                    @endfor
-
-                    <td style="border:1px solid #000; background:#f2f2f2;"></td>
-                    <td style="border:1px solid #000; background:#f2f2f2;"></td>
-                </tr>
-                @endif
-
-                {{-- DATA --}}
-                <tr>
+                    {{-- NO & NAMA - Hanya di chunk pertama --}}
                     @if($chunkIndex === 0)
-                        <td rowspan="{{ $rowSpan }}" style="border:1px solid #000; vertical-align:top; font-weight:bold;">
+                        <td rowspan="{{ $rowSpan }}" style="border:1px solid #000; vertical-align:top; text-align:center; font-weight:bold;">
+                            {{ $userIndex + 1 }}
+                        </td>
+                        <td rowspan="{{ $rowSpan }}" style="border:1px solid #000; vertical-align:top; font-weight:bold; padding-left:5px;">
                             {{ $user->name }}
                         </td>
                     @endif
 
+                    {{-- HEADER TANGGAL (01-Jan, 02-Jan, dst) --}}
+                    @foreach($chunk as $date)
+                        <td style="border:1px solid #000; text-align:center; background:#f2f2f2; font-size:9px; font-weight:bold;">
+                            {{ \Carbon\Carbon::parse($date)->format('d-M') }}
+                        </td>
+                    @endforeach
+
+                    {{-- Padding kalau chunk kurang dari 11 --}}
+                    @for($k = count($chunk); $k < 11; $k++)
+                        <td style="border:1px solid #000; background:#eaeaea;"></td>
+                    @endfor
+
+                    {{-- TOTAL TELAT & LEMBUR - Hanya di chunk pertama --}}
+                    @if($chunkIndex === 0)
+                        <td rowspan="{{ $rowSpan }}" style="border:1px solid #000; vertical-align:top; text-align:center;">
+                            {{ $totalTelat ?: '' }}
+                        </td>
+                        <td rowspan="{{ $rowSpan }}" style="border:1px solid #000; vertical-align:top; text-align:center;">
+                            {{ $totalLemburStr }}
+                        </td>
+                    @endif
+                </tr>
+
+                {{-- ROW DATA ABSENSI --}}
+                <tr>
                     @foreach($chunk as $date)
                         @php
                             $absen = $userAbsensi->first(fn($i) =>
@@ -119,14 +115,16 @@
 
                             if ($absen) {
                                 $in = \Carbon\Carbon::parse($absen->check_in_at)->format('H:i');
-                                $out = $absen->check_out_at ? \Carbon\Carbon::parse($absen->check_out_at)->format('H:i') : '-';
+                                $out = $absen->check_out_at ? \Carbon\Carbon::parse($absen->check_out_at)->format('H:i') : '--';
 
                                 if ($absen->status == 'hadir') {
                                     $cell = "$in - $out";
                                     if ($absen->late_minutes > 0) $cell .= " Tl:{$absen->late_minutes}m";
                                     if ($absen->overtime_minutes > 0) {
                                         $jam = floor($absen->overtime_minutes / 60);
+                                        $menit = $absen->overtime_minutes % 60;
                                         $cell .= " Lb:{$jam}j";
+                                        if ($menit > 0) $cell .= " {$menit}m";
                                     }
                                 } else {
                                     $cell = ucfirst($absen->status);
@@ -139,30 +137,22 @@
                             }
                         @endphp
 
-                        <td style="border:1px solid #000; font-size:10px; text-align:center; {{ $bg }}">
+                        <td style="border:1px solid #000; font-size:9px; text-align:center; {{ $bg }}">
                             {{ $cell }}
                         </td>
                     @endforeach
 
+                    {{-- Padding kalau chunk kurang dari 11 --}}
                     @for($k = count($chunk); $k < 11; $k++)
                         <td style="border:1px solid #000;"></td>
                     @endfor
-
-                    @if($chunkIndex === 0)
-                        <td rowspan="{{ $rowSpan }}" style="border:1px solid #000; vertical-align:top; text-align:center;">
-                            {{ $totalTelat ?: '' }}
-                        </td>
-                        <td rowspan="{{ $rowSpan }}" style="border:1px solid #000; vertical-align:top; text-align:center;">
-                            {{ $totalLemburStr }}
-                        </td>
-                    @endif
                 </tr>
-
             @endforeach
         @endforeach
     </tbody>
 </table>
 
+{{-- Spacing antar table group --}}
 @if($groupIndex < count($dateGroups)-1)
 <table><tr><td style="height:30px;"></td></tr></table>
 @endif
